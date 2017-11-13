8a91c651a7596e22cfed3e522a65a851b6cf5ddf
esraa <esraa.mohamed87@gmail.com>
2017-11-13 02:53:06 +0200

    Applying review comments by fixing queries in newsdb.py and updating html in news_forum.py

--- news_forum.py
+++ news_forum.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 from flask import Flask
 
 from newsdb import get_three_most_popular_articles, get_authors, get_errors
@@ -11,39 +12,54 @@ HTML_WRAP = '''\
   <head>
     <title>News Report</title>
     <style>
-      h1, form { text-align: center; }
-      div.post { border: 1px solid #999;
-                 padding: 10px 10px;
-                 margin: 10px 20%%; }
-      hr.postbound { width: 50%%; }
-      em.date { color: #999 }
+      h1, h2{ text-align: center; }
+      div.post { 
+        border: 1px solid #999;
+        padding: 10px 10px;
+        margin: 10px 20%%; 
+        }
     </style>
   </head>
   <body>
     <h1>News Report</h1>
-    <!-- post content will go here -->
-%s
+    %s
   </body>
 </html>
 '''
 
-POST = '''\
-    <div class=post><em class=date>%s</em><br>%s</div>
+# HTML template for articles data
+ARTICLES = '''\
+    <div class=post><h3>Article Title</h3>%s<br><h3>Views</h3>%s</div>
+'''
+# HTML template for authors data
+AUTHORS = '''\
+    <div class=post><h3>Author name</h3>%s<br><h3>Article name</h3>%s
+    <br><h3>Views</h3>%s</div>
+'''
+# HTML template for error data
+ERRORS = '''\
+    <div class=post><h3>Day</h3>%s<br><h3>Error percentage</h3>%s %%</div>
 '''
 
 
 @app.route('/', methods=['GET'])
-def three_popular_articles():
-    articles = "".join(
-        POST % text for text in get_three_most_popular_articles())
-    authors = "".join(POST % text for text in get_authors())
-    errors = "".join(POST % (date, text) for text, date in get_errors())
-    results = articles + authors + errors
-    html = HTML_WRAP % results
+def main():
+    """
+    Main page of the report.
+    :return:
+    """
+    articles = "".join(ARTICLES % (name, count) for name, count in
+                       get_three_most_popular_articles())
+    authors = "".join(AUTHORS % (name, title, count) for name, title, count in
+                      get_authors())
+    errors = "".join(ERRORS % (date, num) for date, num in get_errors())
+    result = "<h2> 3 Most famous articles</h2>'{0}', " \
+             "<br><h2> Famous authors</h2>'{1}', " \
+             "<br><h2> Error rate was more than 1 percent on:</h2>'{2}'".\
+        format(articles, authors, errors)
+    html = HTML_WRAP % result
     return html
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
-
-
--- newsdb.py
+++ newsdb.py
@@ -1,5 +1,6 @@
-# "Database code" for the DB News.
 # !/usr/bin/env python3
+#  "Database code" for the DB News.
+
 
 import csv
 
@@ -8,16 +9,28 @@ import psycopg2
 DBNAME = "news"
 
 
+def connect(database_name="news"):
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except ConnectionError:
+        print("Database not found, Please check the database name")
+
+
 def get_three_most_popular_articles():
     """Return the 3 most popular articles"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
+    db, cursor = connect(DBNAME)
     query = '''
-    select title, count(*) from log join articles on path like '%' ||slug ||'%' 
-    group by title limit 3;
+    select title, count(*) as views
+    from log join articles
+    on log.path = concat('/article/', articles.slug)
+    group by title
+    order by views desc
+    limit 3;
     '''
-    c.execute(query)
-    result = c.fetchall()
+    cursor.execute(query)
+    result = cursor.fetchall()
 
     with open('results.csv', 'wb') as f:
         writer = csv.writer(f, delimiter='-')
@@ -29,18 +42,17 @@ def get_three_most_popular_articles():
 
 
 def get_authors():
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
+    db, cursor = connect(DBNAME)
     query = '''
-    select authors.name, articleCount from 
+    select authors.name, article.title, articleCount from 
     (select author, title, count(*) as articleCount from articles 
-    join log on path like '%' ||slug ||'%' group by title, 
-     author order by articleCount desc) as article join authors
-    on article.author = authors.id 
-    group by authors.name, articleCount limit 4;
+    join log on log.path = concat('/article/', articles.slug) 
+    group by title, author order by articleCount desc) as article 
+    join authors on article.author = authors.id 
+    group by authors.name, article.title, articleCount limit 4;
     '''
-    c.execute(query)
-    result = c.fetchall()
+    cursor.execute(query)
+    result = cursor.fetchall()
     with open('results.csv', 'a') as f:
         writer = csv.writer(f, delimiter='-')
         for row in result:
@@ -51,8 +63,7 @@ def get_authors():
 
 
 def get_errors():
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
+    db, cursor = connect(DBNAME)
     query = '''SELECT 
     l1.time::timestamp::date, count(l2.id)::float / 
     count(l1.id)::float as error_percentage
@@ -60,14 +71,20 @@ def get_errors():
     public.log as  l1 left join (select * from public.log where status 
     like '%404%') as l2 on l1.id = l2.id
     group by l1.time::timestamp::date
-    having count(l2.id)::float / count(l1.id)::float >= 0.01
+    having count(l2.id)::float / count(l1.id)::float >= 0.01;
     '''
-    c.execute(query)
-    result = c.fetchall()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    lst = []
+    for r in result:
+        date, percentage = r
+        percentage *= 100
+        date = date.strftime('%B, %d %Y')
+        lst.append((date, str(float("%.2f" % percentage))))
     with open('results.csv', 'a') as f:
         writer = csv.writer(f, delimiter='-')
-        for row in result:
+        for row in lst:
             writer.writerow(row)
         writer.writerow("\n")
     db.close()
-    return result
+    return lst
