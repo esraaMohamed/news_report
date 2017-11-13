050a39d21a909e8f47c6275f6360361592c70587
esraa <esraa.mohamed87@gmail.com>
2017-11-13 14:24:24 +0200

    fixing authors query

--- newsdb.py
+++ newsdb.py
@@ -14,7 +14,7 @@ def connect(database_name="news"):
         db = psycopg2.connect("dbname={}".format(database_name))
         cursor = db.cursor()
         return db, cursor
-    except ConnectionError:
+    except (Exception, psycopg2.DatabaseError):
         print("Database not found, Please check the database name")
 
 
@@ -22,21 +22,24 @@ def get_three_most_popular_articles():
     """Return the 3 most popular articles"""
     db, cursor = connect(DBNAME)
     query = '''
-    select title, count(*) as views
-    from log join articles
-    on log.path = concat('/article/', articles.slug)
-    group by title
-    order by views desc
-    limit 3;
+    SELECT title, views
+    FROM articles
+    INNER JOIN
+        (SELECT path, count(path) AS views
+         FROM log
+         GROUP BY log.path) AS log
+    ON log.path = '/article/' || articles.slug
+    ORDER BY views DESC
+    LIMIT 3;
     '''
     cursor.execute(query)
     result = cursor.fetchall()
-
-    with open('results.csv', 'wb') as f:
-        writer = csv.writer(f, delimiter='-')
-        for row in result:
-            writer.writerow(row)
-        writer.writerow("\n")
+    csv.register_dialect('myDialect', delimiter='-')
+    my_file = open('results.csv', 'w')
+    with my_file:
+        writer = csv.writer(my_file, dialect='myDialect')
+        writer.writerows(result)
+        writer.writerows("\n")
     db.close()
     return result
 
@@ -44,20 +47,21 @@ def get_three_most_popular_articles():
 def get_authors():
     db, cursor = connect(DBNAME)
     query = '''
-    select authors.name, article.title, articleCount from 
+    select authors.name, sum(articleCount) from 
     (select author, title, count(*) as articleCount from articles 
     join log on log.path = concat('/article/', articles.slug) 
     group by title, author order by articleCount desc) as article 
     join authors on article.author = authors.id 
-    group by authors.name, article.title, articleCount limit 4;
+    group by authors.name limit 4;
     '''
     cursor.execute(query)
     result = cursor.fetchall()
-    with open('results.csv', 'a') as f:
-        writer = csv.writer(f, delimiter='-')
-        for row in result:
-            writer.writerow(row)
-        writer.writerow("\n")
+    csv.register_dialect('myDialect', delimiter='-')
+    my_file = open('results.csv', 'a')
+    with my_file:
+        writer = csv.writer(my_file, dialect='myDialect')
+        writer.writerows(result)
+        writer.writerows("\n")
     db.close()
     return result
 
@@ -80,11 +84,12 @@ def get_errors():
         date, percentage = r
         percentage *= 100
         date = date.strftime('%B, %d %Y')
-        lst.append((date, str(float("%.2f" % percentage))))
-    with open('results.csv', 'a') as f:
-        writer = csv.writer(f, delimiter='-')
-        for row in lst:
-            writer.writerow(row)
-        writer.writerow("\n")
+        lst.append((date, '{:.2%}'.format(percentage)))
+    csv.register_dialect('myDialect', delimiter='-')
+    my_file = open('results.csv', 'a')
+    with my_file:
+        writer = csv.writer(my_file, dialect='myDialect')
+        writer.writerows(lst)
+        writer.writerows("\n")
     db.close()
     return lst
